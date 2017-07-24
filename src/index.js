d44013bf125564a79fe811c36591aed72fe4639e
Dmitriy Salnikov <ibcWillJust42@gmail.com>
2017-07-24 21:36:03 +0300

    parallel replace

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const replaceInFiles = require('./replaceInFiles.js').default;
 const regexpPathToFiles = '/home/wj42/work/training/replace-in-files/src/sandbox/index.js';
 const optionsForRegexpPathToFiles = {};
 
-const regexp = / *const (.*) = loopback\.getModel\((.*)\);.*\s *const (.*) = \1.create\((.*)\);.*/gm;
+const regexp = /const/g;
 
 function replaceFunction(
   match,
@@ -16,15 +16,15 @@ function replaceFunction(
   file,
   pathToFile
 ) {
-  console.log('|42| ->    match', match);
-  console.log('|42| ->    p1', p1);
-  console.log('|42| ->    p2', p2);
-  console.log('|42| ->    p3', p3);
-  console.log('|42| ->    p4', p4);
-  console.log('|42| ->    offset', offset);
-  console.log('|42| ->    file', file);
-  console.log('|42| ->    pathToFile', pathToFile);
-  return `const ${p3} = createModel(${p2}, ${p4});`;
+  // console.log('|42| ->    match', match);
+  // console.log('|42| ->    p1', p1);
+  // console.log('|42| ->    p2', p2);
+  // console.log('|42| ->    p3', p3);
+  // console.log('|42| ->    p4', p4);
+  // console.log('|42| ->    offset', offset);
+  // console.log('|42| ->    file', file);
+  // console.log('|42| ->    pathToFile', pathToFile);
+  return 'var';
 }
 
 
--- src/replaceInFiles.js
+++ src/replaceInFiles.js
@@ -4,72 +4,48 @@ const helpers = require('./libs/helpers');
 
 const defaultOptionsForRegexpPathToFiles = {
   ignore: [
-    '**/node_modules/**',
+    // '**/node_modules/**',
   ],
   nodir: true,
 };
 
-exports.default = function replaceInFiles({
-  regexpPathToFiles,
-  optionsForRegexpPathToFiles,
-  regexp,
-  replaceFunction,
-}) {
+exports.default = function replaceInFiles(option) {
   co(function* () {
+    const {
+      regexpPathToFiles,
+      optionsForRegexpPathToFiles,
+      regexp,
+      replaceFunction,
+    } = option;
+
     const options = _.merge(defaultOptionsForRegexpPathToFiles, optionsForRegexpPathToFiles);
 
     const paths = yield helpers.pGlob(regexpPathToFiles, options);
     helpers.log(`finded: ${paths.length} paths`);
 
-    const preparedToWrite = yield prepareToRewrite(paths, regexp, replaceFunction);
-
-    yield _.map(preparedToWrite, item => helpers.pWriteFile(item.path, item.changedData));
+    yield prepareToRewrite(paths, regexp, replaceFunction, option);
   }).catch(helpers.handleError);
 };
 
+function* replaceInFile(path, option) {
+  const {
+    regexp,
+    replaceFunction,
+  } = option;
+  const data = yield helpers.pReadFile(path);
 
-function getPreparedToRewrite({ path, data }, regexp, replaceFunction) {
-  const changedData = data.replace(regexp, replaceFunction);
-  return {
-    path,
-    changedData,
-  };
+  const updated = getPreparedToRewrite(path, data, regexp, replaceFunction);
+
+  yield helpers.pWriteFile(path, updated);
 }
 
-function curryGetPreparedToRewrite(regexp, replaceFunction, matchLogs) {
-  return (item) => {
-    const path = item[0];
-    const data = item[1];
-
-    const replaceFunctionWithLogging = decorateReplaceFunction(replaceFunction, matchLogs, path);
-
-    return getPreparedToRewrite({ path, data }, regexp, replaceFunctionWithLogging);
-  };
+function getPreparedToRewrite(path, data, regexp, replaceFunction) {
+  const updated = data.replace(regexp, replaceFunction);
+  return updated;
 }
 
-function* prepareToRewrite(paths, regexp, replaceFunction) {
-  const datasets = yield _.map(paths, path => helpers.pReadFile(path));
-  const zipped = _.zip(paths, datasets);
-
-  const matchLogs = {};
-  const curredGetPreparedToRewrite = curryGetPreparedToRewrite(regexp, replaceFunction, matchLogs);
-  const preparedToWrite = _.map(zipped, curredGetPreparedToRewrite);
-
-  showMatchLogs(matchLogs);
+function* prepareToRewrite(paths, regexp, replaceFunction, option) {
+  const preparedToWrite = yield _.map(paths, path => replaceInFile(path, option));
 
   return preparedToWrite;
-}
-
-function decorateReplaceFunction(replaceFunction, matchLogs, path) {
-  return (...args) => {
-    // eslint-disable-next-line no-param-reassign
-    matchLogs[path] = matchLogs[path] ? ++matchLogs[path] : 1;
-    return replaceFunction(...args, path);
-  };
-}
-
-function showMatchLogs(matchLogs) {
-  _.forEach(matchLogs, (value, key) => {
-    helpers.log(`${key}  :  ${value} matches`);
-  });
 }
