bbf56f90c832a045bf73cbe3e30530fffce117c4
Dmitriy Salnikov <ibcWillJust42@gmail.com>
2017-08-10 20:52:20 +0300

    tests Configurator

--- __tests__/lib/Configurator.test.js
+++ __tests__/lib/Configurator.test.js
@@ -28,4 +28,151 @@ describe('lib/Configurator.js', () => {
     expect(Configurator.prepareReplaceConfig).toHaveBeenCalledTimes(1);
     expect(Configurator.prepareReplaceConfig).toHaveBeenCalledWith(options);
   });
+  describe('prepareFindFilesConfig', () => {
+    test('1', () => {
+      const Configurator = require('../../lib/Configurator.js');
+      const config = require('../../lib/config.json');
+      config.Configurator.defaultOptionsForFiles = {
+        ignore: [
+          '/ignore/first/path',
+          '**/ingonre/regexp.*'
+        ],
+        foo: 'bar'
+      };
+      const options = {
+        another: 'field',
+        files: 'pathToFiles',
+        optionsForFiles: {
+          key1: 'value1',
+          key2: 'value2'
+        }
+      };
+
+      const result = Configurator.prepareFindFilesConfig(options);
+
+      expect(result).toEqual({
+        files: 'pathToFiles',
+        optionsForFiles: {
+          ignore: [
+            '/ignore/first/path',
+            '**/ingonre/regexp.*'
+          ],
+          foo: 'bar',
+          key1: 'value1',
+          key2: 'value2'
+        }
+      });
+    });
+    test('2', () => {
+      const Configurator = require('../../lib/Configurator.js');
+      const config = require('../../lib/config.json');
+      config.Configurator.defaultOptionsForFiles = {
+        ignore: [
+          '/ignore/first/path',
+          '**/ingonre/regexp.*'
+        ],
+        foo: 'bar',
+        qwerty: 123
+      };
+      const options = {
+        another: 'field',
+        files: 'pathToFiles',
+        optionsForFiles: {
+          ignore: [
+            'new/path/instead/default/paths'
+          ],
+          key1: 'value1',
+          foo: 'newBar'
+        }
+      };
+
+      const result = Configurator.prepareFindFilesConfig(options);
+
+      expect(result).toEqual({
+        files: 'pathToFiles',
+        optionsForFiles: {
+          ignore: [
+            'new/path/instead/default/paths'
+          ],
+          key1: 'value1',
+          foo: 'newBar',
+          qwerty: 123
+        }
+      });
+    });
+    test('3', () => {
+      const Configurator = require('../../lib/Configurator.js');
+      const config = require('../../lib/config.json');
+      config.Configurator.defaultOptionsForFiles = {
+        ignore: [
+          '/ignore/first/path',
+          '**/ingonre/regexp.*'
+        ],
+        foo: 'bar',
+        qwerty: 123
+      };
+      const options = {
+        another: 'field',
+        files: 'pathToFiles',
+        optionsForFiles: {
+          ignore: [],
+          key1: 'value1',
+          foo: 'newBar'
+        }
+      };
+
+      const result = Configurator.prepareFindFilesConfig(options);
+
+      expect(result).toEqual({
+        files: 'pathToFiles',
+        optionsForFiles: {
+          ignore: [],
+          key1: 'value1',
+          foo: 'newBar',
+          qwerty: 123
+        }
+      });
+    });
+  });
+  describe('prepareReplaceConfig', () => {
+    test('1', () => {
+      const Configurator = require('../../lib/Configurator.js');
+      const config = require('../../lib/config.json');
+      config.Configurator.defaultEncoding = 'utf16';
+      const options = {
+        another: 'field',
+        files: 'pathToFiles',
+        from: 'from',
+        to: 'to',
+        encoding: 'utf32'
+      };
+
+      const result = Configurator.prepareReplaceConfig(options);
+
+      expect(result).toEqual({
+        from: 'from',
+        to: 'to',
+        encoding: 'utf32'
+      });
+    });
+    test('2', () => {
+      const Configurator = require('../../lib/Configurator.js');
+      const config = require('../../lib/config.json');
+      config.Configurator.defaultEncoding = 'utf16';
+      const options = {
+        another: 'field',
+        files: 'pathToFiles',
+        from: 'from',
+        to: 'to',
+      };
+
+      const result = Configurator.prepareReplaceConfig(options);
+
+      expect(result).toEqual({
+        from: 'from',
+        to: 'to',
+        encoding: 'utf16'
+      });
+    });
+  });
 });
